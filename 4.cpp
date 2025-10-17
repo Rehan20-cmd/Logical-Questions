c0d9ebcd2cc553a023e55563675d7dc33636a818
Rehan20-cmd <itxrehan75@gmail.com>
2025-10-17 22:03:33 +0500

    Fibonacci Series
    Fibonacci Series

---
+++ 4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+#include<string.h>
+int main(){
+int n1=0,n2=1,n3;
+for(int i=0;i<10;i++){
+
+  n3=n1+n2;
+ 
+  cout<<n3<<endl;
+   n1=n2;
+  n2=n3;
+
+}
+
+
+return 0;
+}
+
