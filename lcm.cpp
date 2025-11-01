32825265d7fba782cab39b3862d5d4c7361be50b
Rehan20-cmd <itxrehan75@gmail.com>
2025-11-01 19:26:50 +0500

    Add files via upload

---
+++ lcm.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int r,a=20,b=16,lcf;
+
+
+int main(){
+    while(true){
+        lcf=a;
+r=a%b;
+
+if(r==0){
+break;
+}
+a=a*2;
+
+}
+cout<<"Lcf is "<<lcf;
+        return 0;
+}
